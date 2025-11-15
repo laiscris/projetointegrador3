aa087c8c6c10207afc0a178bd46a407d4df18c3c
lais <laiscristina0009@gmail.com>
2025-11-15 15:31:47 -0300

    colocando o @cache_data

--- dashboard-combustiveis/filtros.py
+++ dashboard-combustiveis/filtros.py
@@ -1,19 +1,23 @@
 import pandas as pd
+import streamlit as st
 
 # Datasets
 df_tratado = pd.read_csv('precos_tratado.csv')
 
 # Filtra de acordo com a escolha de combustível
+@st.cache_data
 def filtra_combustivel(escolha_combustivel):
     return df_tratado[df_tratado.PRODUTO.isin(escolha_combustivel)]
 
 # Filtra de acordo com a escolha de ano
+@st.cache_data
 def filtra_ano(df_filtra_combustivel, escolha_ano):
     inicio = escolha_ano[0]
     fim = escolha_ano[1]
     return df_filtra_combustivel[(df_filtra_combustivel.ANO_SEP >= inicio) & (df_filtra_combustivel.ANO_SEP <= fim)].copy()
 
 # Filtra de acordo com a escolha do mes
+@st.cache_data
 def filtra_mes(df_filtra_ano, escolha_mes):
     if escolha_mes is None:
         return df_filtra_ano
@@ -24,6 +28,7 @@ def filtra_mes(df_filtra_ano, escolha_mes):
     return df_filtra_ano[(df_filtra_ano.MÊS_SEP >= inicio) & (df_filtra_ano.MÊS_SEP <= fim)].copy()
 
 # Filtra o dataset inteiro de acordo com a escolha de combustível e períodos de ano e mês
+@st.cache_data
 def filtra_dataset(combustivel, ano, mes):
 
     try:
